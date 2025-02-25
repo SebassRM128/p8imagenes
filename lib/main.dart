79cb6eb3c17577489e2cd4199da0ff6dd6ff3eb7
GAEL_SEBASTIAN_ROJAS_MATA <a.22308051281303@cbtis128.edu.mx>
2025-02-25 16:58:34 +0000

    enviar

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,64 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MiImagen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiImagen extends StatelessWidget {
+  const MiImagen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Mis imagenes",
+              style: TextStyle(
+                fontSize: 30,
+                fontStyle: FontStyle.italic,
+                color: Color(0xffffffff),
+              )),
+          backgroundColor: Color(0xff6e9570),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Gael Sebastian Rojas Mata 22308051281303",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontStyle: FontStyle.italic,
+                    color: Color(0xff3d463d),
+                  )),
+              Image.network(
+                'https://raw.githubusercontent.com/SebassRM128/imagenes-para-APP-flutter/refs/heads/main/reloj1.jpg',
+                width: 200, // Ajusta el ancho según sea necesario
+                height: 200, // Ajusta la altura según sea necesario
+              ),
+              SizedBox(height: 20), // Espacio entre las imágenes
+              Text(
+                'RELOJ CARTIER en github',
+                style: TextStyle(
+                    fontSize: 16,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black),
+              ),
+              Image.network(
+                'https://raw.githubusercontent.com/SebassRM128/imagenes-para-APP-flutter/refs/heads/main/reloj2.jpg',
+                width: 200, // Ajusta el ancho según sea necesario
+                height: 200, // Ajusta la altura según sea necesario
+              ),
+              SizedBox(height: 20), // Espacio entre las imágenes
+              Text(
+                'RELOJ CUAHTLI en github',
+                style: TextStyle(
+                    fontSize: 16,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black),
+              ),
+            ],
+          ),
         ),
       ),
     );
-  }
-}
+  } //witgets
+} //clase MiImageb
